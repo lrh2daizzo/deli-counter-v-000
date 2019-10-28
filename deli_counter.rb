8e61f529ca949a540b8dcd2bedd1951af9935fcb
Luis Herrera <herrera.lrh@gmail.com>
2019-10-28 18:34:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,5 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{katz_deli[i]} in line."
+  puts "Welcome, #{name}. You are number in line."
 end
